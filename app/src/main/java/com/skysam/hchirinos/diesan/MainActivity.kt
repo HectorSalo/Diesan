51f8c3b18b3914b8cd3946c834461e22f8711981
Prueba <hectorsalomonchirinos@gmial.com>
2021-12-27 16:47:12 -0400

    V1.0.1 Implementacion vistas

--- app/src/main/java/com/skysam/hchirinos/diesan/MainActivity.kt
+++ app/src/main/java/com/skysam/hchirinos/diesan/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.skysam.hchirinos.diesan
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.skysam.hchirinos.diesan.databinding.ActivityMainBinding
+import com.skysam.hchirinos.diesan.ui.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +48,13 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_settings -> startActivity(Intent(this, SettingsActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/skysam/hchirinos/diesan/ui/lots/FirstFragment.kt
+++ app/src/main/java/com/skysam/hchirinos/diesan/ui/lots/FirstFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.skysam.hchirinos.diesan.ui.lots.databinding.FragmentFirstBinding
+import com.skysam.hchirinos.diesan.R
+import com.skysam.hchirinos.diesan.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
