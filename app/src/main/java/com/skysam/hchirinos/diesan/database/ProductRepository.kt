23d6d70f4d8ee1c801ad29adca7a4ba31a07f0cd
Prueba <hectorsalomonchirinos@gmial.com>
2022-01-19 02:54:03 -0400

    V1.0.7 Implementacion Upload Image

--- app/src/main/java/com/skysam/hchirinos/diesan/database/ProductRepository.kt
+++ app/src/main/java/com/skysam/hchirinos/diesan/database/ProductRepository.kt
@@ -1,8 +1,37 @@
 package com.skysam.hchirinos.diesan.database
 
+import android.net.Uri
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import com.skysam.hchirinos.diesan.common.Constants
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
 /**
  * Created by Hector Chirinos on 18/01/2022.
  */
 object ProductRepository {
+    private const val PATH = "${Constants.PRODUCTS}/"
 
+    private fun getInstanceStorage(): StorageReference {
+        return Firebase.storage.reference.child(PATH)
+    }
+
+    fun uploadImage(uri: Uri): Flow<String> {
+        return callbackFlow {
+            val request = getInstanceStorage()
+                .child(uri.lastPathSegment!!)
+                .putFile(uri)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        offer(getInstanceStorage().child(uri.lastPathSegment!!)
+                            .downloadUrl.toString())
+                    } else {
+                        offer("Error")
+                    }
+                }
+        }
+    }
 }
--- app/src/main/java/com/skysam/hchirinos/diesan/ui/MainViewModel.kt
+++ app/src/main/java/com/skysam/hchirinos/diesan/ui/MainViewModel.kt
@@ -1,13 +1,16 @@
 package com.skysam.hchirinos.diesan.ui
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.skysam.hchirinos.diesan.database.ProductRepository
 
 class MainViewModel : ViewModel() {
+    private val _urlDownload = MutableLiveData<String>()
+    val urlDownload: LiveData<String> get() = _urlDownload
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun uploadImage(uri: Uri): LiveData<String> {
+        return ProductRepository.uploadImage(uri).asLiveData()
     }
-    val text: LiveData<String> = _text
 }
